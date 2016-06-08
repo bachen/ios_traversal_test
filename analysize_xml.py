1281bcd5538c2ded88f16a2d1124cfdaad0779ec
jenkins <jenkins@nalideMacBook-Pro-6.local>
2016-06-08 10:43:34 +0800

    update xml

--- analysize_xml.py
+++ analysize_xml.py
@@ -6,31 +6,46 @@ import xml.dom.minidom
 
 
 def get_device_number(filename='./config.xml'):
-    """
+	"""
 
     :type filename: xml
     """
-    xml_doc = xml.dom.minidom.parse(filename)
-    root = xml_doc.documentElement
-    nodes = root.getElementsByTagName('device')
-    device_number = len(nodes)
-    return device_number
+	xml_doc = xml.dom.minidom.parse(filename)
+	root = xml_doc.documentElement
+	nodes = root.getElementsByTagName('device')
+	device_number = len(nodes)
+	return device_number
 
 
 def get_config(filename='./config.xml', device_number=None):
-    """
+	"""
 
     :type filename: xml
     :type device_number: int
     """
-    xml_doc = xml.dom.minidom.parse(filename)
-    root = xml_doc.documentElement
-    nodes = root.getElementsByTagName('device')
-    for node in nodes:
-        if node.getAttribute('number') == str(device_number):
-            ios_version = node.childNodes[2].data
-            bundle_id = node.childNodes[3].data
-            device_type = node.childNodes[4].data
-    nodes = root.getElementsByTagName('level')
-    level = int(nodes[0].data)
-    return ios_version, bundle_id, device_type, level
+	xml_doc = xml.dom.minidom.parse(filename)
+	root = xml_doc.documentElement
+	# get version
+	versions = root.getElementsByTagName('version')
+	version = versions[device_number]
+	ios_version = version.firstChild.data
+	# get bundle_id
+	bundleids = root.getElementsByTagName('bundleid')
+	bundleid = bundleids[device_number]
+	bundle_id = bundleid.firstChild.data
+	# get device type
+	devicetypes = root.getElementsByTagName('type')
+	devicetype = devicetypes[device_number]
+	device_type = devicetype.firstChild.data
+	# get level
+	traversals = root.getElementsByTagName('traversal')
+	levels = traversals[0].getElementsByTagName('level')
+	level = int(levels[0].firstChild.nodeValue)
+	return ios_version, bundle_id, device_type, level
+
+
+if __name__ == '__main__':
+	res1 = get_device_number('config.xml')
+	print res1
+	res2 = get_config('config.xml', 1)
+	print res2
--- main.py
+++ main.py
@@ -8,11 +8,14 @@ from analysize_xml import get_device_number
 device_number = get_device_number(filename='./config.xml')
 
 for i in xrange(0, device_number, 1):
+
     # start up appium program
     # according to device args, create driver object
-    ios_version, bundle_id, device_number, device_type, level = get_config(filename='./config.xml', device_number=i)
+    ios_version, bundle_id, device_type, level = get_config(filename='./config.xml', device_number=i)
     driver = remotedriver(bundle_id=bundle_id, device_type=device_type, ios_version=ios_version)
-# start to traversal app
-# traversal: step 1 get current page source as xml
-# traversal: step 2 discover node which could be clicked
-# traversal: step 3 decide the sort to traversal all node
+	# start to traversal app
+	# traversal: step 1 get current page source as xml
+	# traversal: step 2 discover node which could be clicked
+	# traversal: step 3 decide the sort to traversal all node
+	driver.quit()
+
