727a5d158aff66cdcb5fe8d1c127be1872f751cd
jenkins <jenkins@nalideMacBook-Pro-6.local>
2016-06-20 18:23:30 +0800

    update xml

--- analysize_nodes.py
+++ analysize_nodes.py
@@ -1,54 +1,75 @@
 # -*-coding:utf-8-*=
 import sys
 sys.path.append('/usr/local/lib/python2.7/site-packages')
+from xml.dom.minidom import parseString
 from xml.dom.minidom import parse
 
 
-def find_nodes(xml_doc, filter, click_config, input_config):
+def find_nodes(xml_res, click_config, input_config):
+    xml_doc= parseString(xml_res)
     root = xml_doc.documentElement
+    nodes = []
     # get click enable elements:
     if 'UIAImage' in click_config:
-        UIAImages = root.getElementsByTagName('UIAImage')
+        nodes = xml_2_xpath(root, 'UIAImage')
+    '''
     if 'UIAButton' in click_config:
-        UIAButtons = root.getElementsByTagName('UIAButton')
+        UIAButtons = xml_2_xpath(root, 'UIAButton')
     if 'UIASwitch' in click_config:
-        UIASwitchs = root.getElementsByTagName('UIASwitch')
+        UIASwitchs = xml_2_xpath(root, 'UIASwitch')
     if 'UIATableCell' in click_config:
-        UIATableCells = root.getElementsByTagName('UIATableCell')
+        UIATableCells = xml_2_xpath(root, 'UIATableCell')
     if 'UIAPickerWheel' in click_config:
-        UIAPickerWheels = root.getElementsByTagName('UIAPickerWheel')
+        UIAPickerWheels = xml_2_xpath(root, 'UIAPickerWheel')
     if 'UIACollectionCell' in click_config:
-        UIACollectionCells = root.getElementsByTagName('UIACollectionCell')
+        UIACollectionCells = xml_2_xpath(root, 'UIACollectionCell')
     if 'UIAStaticText' in click_config:
-        UIAStaticTexts = root.getElementsByTagName('UIAStaticText')
+        UIAStaticTexts = xml_2_xpath(root, 'UIAStaticText')
     # get input enable elements:
     if 'UIATextField' in input_config:
-        UIATextFields = root.getElementsByTagName('UIATextField')
+        UIATextFields = xml_2_xpath(root, 'UIATextField')
     if 'UIASearchBar' in input_config:
-        UIASearchBars = root.getElementsByTagName('UIASearchBar')
+        UIASearchBars = xml_2_xpath(root, 'UIASearchBar')
     if 'UIASecureTextField' in input_config:
-        UIASecureTextFields = root.getElementsByTagName('UIASecureTextField')
+        UIASecureTextFields = xml_2_xpath(root, 'UIASecureTextField')
+    '''
     return nodes
 
 
 def get_nodes_config(filename='./node.xml'):
     xml_doc = parse(filename)
     root = xml_doc.documentElement
-    # get nodes filter strategy
-    filters = root.getElementsByTagName('filter')
-    filter = filters[0].firstChild.data
     # get click controllers
     click_configs = root.getElementsByTagName('click')
-    clicks = click_configs.getElementsByTagName('class')
+    clicks = click_configs[0].getElementsByTagName('class')
     click_config = []
     for click in clicks:
         click_config.append(click.firstChild.data)
     # get input controllers
     input_configs = root.getElementsByTagName('input')
-    inputs = input_configs.getElementsByTagName('class')
+    inputs = input_configs[0].getElementsByTagName('class')
     input_config = []
     for input_i in inputs:
         input_config.append(input_i.firstChild.data)
-    return filter, click_config, input_config
+    return click_config, input_config
 
 
+def xml_2_xpath(root, element_type):
+    uia_elements = root.getElementsByTagName(element_type)
+    # elements collect all nodes' xpath which enabled = true and visible = true
+    elements = []
+    for element in uia_elements:
+        if element.getAttribute('enabled') == "true" and element.getAttribute('visible') == "true":
+            element_xpath = ''
+            # element_path is raw path get from xml object, such as '/0/0/1/2'
+            element_path = element.getAttribute('path').split('/')
+            while element_path[-1] != '':
+                element_xpath = '/' + element.nodeName + '[' + element_path[-1] + ']' + element_xpath
+                element = element.parentNode
+                element_path.pop()
+            elements.append(element_xpath)
+    return elements
+
+
+if __name__ == '__main__':
+    print get_nodes_config()
--- traversal.py
+++ traversal.py
@@ -4,6 +4,7 @@ sys.path.append('/usr/local/lib/python2.7/site-packages')
 from ios import remotedriver
 from time import sleep
 from analysize_nodes import find_nodes
+from analysize_nodes import get_nodes_config
 
 
 def traversal(dr, level):
@@ -14,7 +15,9 @@ def traversal(dr, level):
 def get_current_page_all_nodes(dr):
     # get all nodes in current page
     xml_res = dr.page_source
-    enable_nodes = find_nodes(xml_res)
+    print xml_res
+    click_config, input_config = get_nodes_config(filename='./node.xml')
+    enable_nodes = find_nodes(xml_res=unicode(xml_res), click_config=click_config, input_config=input_config)
     return enable_nodes
 
 
@@ -22,5 +25,6 @@ if __name__ == '__main__':
     driver = remotedriver(bundle_id='com.gemd.iting', device_type='iPhone 5', ios_version='9.3.2')
     sleep(10)
     res = get_current_page_all_nodes(driver)
+    sleep(10)
     driver.quit()
     print res
